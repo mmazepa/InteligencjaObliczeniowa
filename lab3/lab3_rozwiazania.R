116a37847721c4c6a56bbeb88dd5cc0de86922ba
mmazepa <maksymilianmariusz@gmail.com>
2018-10-23 00:07:55 +0200

    Wrzutka z lab3  (16.10.2018).

---
+++ lab3/lab3_rozwiazania.R
@@ -0,0 +1,93 @@
+# LABORATORIUM 3
+# -----------------------------------------------------------------------
+
+# ZADANIE 1
+
+# a)
+
+#install.packages("genalg")
+#library(genalg)
+
+duzyProblemPlecakowy <- data.frame(wartosc = sample(10:100,30), waga = sample(10:100,30))
+
+duzyLimit <- 600
+
+fitnessFunc2 <- function(chr)
+{
+  calkowita_wartosc_chr <- chr %*% duzyProblemPlecakowy$wartosc
+  calkowita_waga_chr <- chr %*% duzyProblemPlecakowy$waga
+  if (calkowita_waga_chr > duzyLimit)
+      return(0)
+  else
+    return(-calkowita_wartosc_chr)
+}
+
+duzyPlecakGenAlg <- rbga.bin(size = 30, popSize = 200, iters = 50,
+           mutationChance = 0.03, elitism = T, evalFunc = fitnessFunc2)
+
+# b)
+
+chartData <- data.frame(srednia = -duzyPlecakGenAlg$mean, maksymalne = -duzyPlecakGenAlg$best)
+
+# c)
+
+plot(chartData$maksymalne, type="l", xlab="pokolenie", ylab="fitness (ocena)", col="red")
+title("Działanie Alg. Genetycznego")
+
+# d)
+
+lines(chartData$srednia, col="blue")
+
+# e)
+
+legend("bottomright", c("srednia", "maksymalnie"), lty=c(1,1), col=c("blue", "red"), title = "legenda")
+
+# ZADANIE 2
+
+wielkosc_populacji <- 200
+liczba_iteracji <- 100
+ilosc_przedmiotow1 <- c(60, 120, 200)
+
+duzyProblemPlecakowy1 <- data.frame(wartosc = sample(10:200,60), waga = sample(10:100,60))
+duzyProblemPlecakowy2 <- data.frame(wartosc = sample(10:200,120), waga = sample(10:100,120))
+duzyProblemPlecakowy3 <- data.frame(wartosc = sample(10:200,200), waga = sample(10:100,200))
+
+fitnessFunc3 <- function(chr)
+{
+  calkowita_wartosc_chr <- chr %*% duzyProblemPlecakowy1$wartosc
+  calkowita_waga_chr <- chr %*% duzyProblemPlecakowy1$waga
+  if (calkowita_waga_chr > duzyLimit)
+    return(0)
+  else
+    return(-calkowita_wartosc_chr)
+}
+
+fitnessFunc4 <- function(chr)
+{
+  calkowita_wartosc_chr <- chr %*% duzyProblemPlecakowy2$wartosc
+  calkowita_waga_chr <- chr %*% duzyProblemPlecakowy2$waga
+  if (calkowita_waga_chr > duzyLimit)
+    return(0)
+  else
+    return(-calkowita_wartosc_chr)
+}
+
+fitnessFunc5 <- function(chr)
+{
+  calkowita_wartosc_chr <- chr %*% duzyProblemPlecakowy3$wartosc
+  calkowita_waga_chr <- chr %*% duzyProblemPlecakowy3$waga
+  if (calkowita_waga_chr > duzyLimit)
+    return(0)
+  else
+    return(-calkowita_wartosc_chr)
+}
+
+duzyPlecakGenAlg2p1 <- rbga.bin(size = 200, popSize = 60, iters = 100,
+                             mutationChance = 0.03, elitism = T, evalFunc = fitnessFunc3)
+
+duzyPlecakGenAlg2p1 <- rbga.bin(size = 200, popSize = 120, iters = 100,
+                                mutationChance = 0.03, elitism = T, evalFunc = fitnessFunc4)
+
+duzyPlecakGenAlg2p1 <- rbga.bin(size = 200, popSize = 200, iters = 100,
+                                mutationChance = 0.03, elitism = T, evalFunc = fitnessFunc5)
+# -----------------------------------------------------------------------
