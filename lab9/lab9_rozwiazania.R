c074f34dacbec87767e416657988fbc3be3678de
mmazepa <maksymilianmariusz@gmail.com>
2018-11-28 02:29:07 +0100

    Lab9: zadania 1 i 2 (poprawki).

--- lab9/lab9_rozwiazania.R
+++ lab9/lab9_rozwiazania.R
@@ -7,30 +7,30 @@ ludzie <- data.frame("wiek" = c(23, 25, 28, 22, 46, 50, 48),
                      "waga" = c(75, 67, 120, 65, 70, 68, 97),
                      "wzrost" = c(176, 180, 175, 165, 187, 180, 178),
                      "gra" = c(TRUE, TRUE, FALSE, TRUE, TRUE, FALSE, FALSE))
+ludzie
 
-w1 <- -0.46122
-w2 <- 0.97314
-w3 <- -0.39203
-w4 <- 0.78548
-w5 <- 2.10584
-w6 <- -0.57847
-b1 <- 0.80109
-b2 <- 0.43529
-b3 <- -0.2368
-v1 <- -0.81546
-v2 <- 1.03775
+w <- c(-0.46122, 0.97314, -0.39203, 0.78548, 2.10584, -0.57847)
+v <- c(-0.81546, 1.03775)
+b <- c(0.80109, 0.43529, -0.2368)
+
+activate <- function(x) {
+  return(1/(1+exp(-x)))
+}
 
 forwardPass <- function(wiek, waga, wzrost) {
-  hidden1 <- wiek * w1 + wzrost * w2 + waga * w3 + b1
-  hidden1 <- 1/(1+exp(-hidden1))
-  hidden2 <- wiek * w4 + wzrost * w5 + waga * w6 + b2
-  hidden2 <- 1/(1+exp(-hidden2))
-  output <- v1 * hidden1 + v2 * hidden2 + b3
+  hidden1 <- activate((wiek * w[1]) + (waga * w[2]) + (wzrost * w[3]) + b[1])
+  hidden2 <- activate((wiek * w[4]) + (waga * w[5]) + (wzrost * w[6]) + b[2])
+  output <- (v[1] * hidden1) + (v[2] * hidden2) + b[3]
   return(output)
 }
 
-forwardPass(23, 75, 176)
-forwardPass(50, 68, 180)
+forwarded <- c()
+for (row in 1:nrow(ludzie)) {
+  tmp <- forwardPass(ludzie[row,][1], ludzie[row,][2], ludzie[row,][3])
+  forwarded <- c(forwarded, tmp)
+}
+forwarded <- data.frame("forwarded" = as.numeric(forwarded))
+forwarded
 
 # ___ ZADANIE 2 __________________________________________________________
 
@@ -39,33 +39,52 @@ norm <- function(x)
 {
   (x-min(x))/(max(x)-min(x))
 }
-iris.data.norm <- data.frame(norm(iris.data[1]), norm(iris.data[2]),
-                             norm(iris.data[3]), norm(iris.data[4]),
-                             iris.data[5])
+iris.norm <- data.frame(norm(iris.data[1]), norm(iris.data[2]),
+                        norm(iris.data[3]), norm(iris.data[4]),
+                        iris.data[5])
 
 set.seed(1234)
 ind <- sample(2, nrow(iris), replace=TRUE, prob=c(0.67, 0.33))
-iris.norm.training <- iris.data.norm[ind==1,1:5]
-iris.norm.test <- iris.data.norm[ind==2,1:5]
+iris.train <- iris.norm[ind==1,1:5]
+iris.test <- iris.norm[ind==2,1:5]
 
 #install.packages("neuralnet")
 #library(neuralnet)
 
-#neuralnet(iris.norm.training)
+iris.train$Setosa <- 0
+iris.train$Versicolor <- 0
+iris.train$Virginica <- 0
 
-iris.norm.training$Setosa <- 0
-iris.norm.training$Versicolor <- 0
-iris.norm.training$Virginica <- 0
-
-for (row in 1:nrow(iris.norm.training)) {
-  if (iris.norm.training[row,]["Species"] == "setosa") iris.norm.training[row,]["Setosa"] = 1
-  if (iris.norm.training[row,]["Species"] == "versicolor") iris.norm.training[row,]["Versicolor"] = 1
-  if (iris.norm.training[row,]["Species"] == "virginica") iris.norm.training[row,]["Virginica"] = 1
+for (row in 1:nrow(iris.train)) {
+  if (iris.train[row,]["Species"] == "setosa") iris.train[row,]["Setosa"] = 1
+  if (iris.train[row,]["Species"] == "versicolor") iris.train[row,]["Versicolor"] = 1
+  if (iris.train[row,]["Species"] == "virginica") iris.train[row,]["Virginica"] = 1
 }
 
-iris.norm.training <- subset(iris.norm.training, select = -c(Species))
-iris.neuralnet <- neuralnet(Setosa + Versicolor + Virginica ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width, iris.norm.training)
-compute(iris.norm.test)
+iris.train <- subset(iris.train, select = -c(Species))
+iris.neuralnet <- neuralnet(Setosa + Versicolor + Virginica ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width,
+                            iris.train, hidden=4)
+iris.pred <- compute(iris.neuralnet, iris.test[,1:4])
+#plot(iris.neuralnet)
+
+iris.pred_species <- c()
+for (row in 1:nrow(iris.pred$net.result)) {
+  col_number <- match(max(iris.pred$net.result[row,]), iris.pred$net.result[row,])
+  if (col_number == 1) iris.pred_species <- c(iris.pred_species, "setosa")
+  if (col_number == 2) iris.pred_species <- c(iris.pred_species, "versicolor")
+  if (col_number == 3) iris.pred_species <- c(iris.pred_species, "virginica")
+}
+
+iris.comparison <- cbind("real" = as.character(iris.test["Species"][,1]), "predicted" = iris.pred_species)
+
+iris.result <- c()
+for (row in 1:nrow(iris.comparison)) {
+  if (iris.comparison[,1][row] == iris.comparison[,2][row]) iris.result <- c(iris.result, TRUE)
+  else iris.result <- c(iris.result, FALSE)
+}
+
+accuracy <- (as.numeric(table(iris.result)["TRUE"])/40)*100
+accuracy
 
 # ___ ZADANIE 3 __________________________________________________________
 
